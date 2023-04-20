fbd1806046fc749fd3c8d9c903baa08c8da2952d
JosueDeLosSantos <jdd1993@hotmail.com>
2023-04-19 22:35:36 -0400

    Start 'knightMoves' function

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 import * as helper from "./help.js";
 
-console.log(helper.knight([4, 4]));
+function knightMoves(sSquare, eSquare) {
+  let a = helper.knight(sSquare);
+  let b = helper.knight(eSquare);
+  if (a.data[0] == 0 || a.data[1] == 0) a = helper.knight([1, 2]);
+  if (b.data[0] == 0 || b.data[1] == 0) b = helper.knight([1, 2]);
+  console.log(a);
+  console.log(b);
+}
+
+knightMoves([0, 8], [5, 0]);
